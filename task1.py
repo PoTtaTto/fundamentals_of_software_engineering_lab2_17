eeb7376753b965cc68e952ae3ad45ac41204074b
PoTtaTto <MrPotatsus@yandex.ru>
2024-03-12 21:39:18 +0300

    hard task is completed

--- task1.py
+++ task1.py
@@ -4,34 +4,10 @@
 
 # Standard
 import os
-import argparse
+import click
 from random import randint
 import json
 import jsonschema
-
-
-def add_train(trains, num, destination, start_time):
-    """
-    Добавляет информацию о поезде в список trains.
-
-    Args:
-    - trains (list): Список поездов.
-    - num (int): Номер поезда.
-    - destination (str): Пункт назначения.
-    - start_time (str): Время отправки
-
-    Returns:
-    - trains (list): Список поездов.
-    """
-    trains.append({
-        'num': num,
-        'destination': destination,
-        'start_time': start_time
-    })
-    if len(trains) > 1:
-        trains.sort(key=lambda item: item['start_time'])
-
-    return trains
 
 
 def save_trains(file_name, trains):
@@ -58,21 +34,63 @@ def load_trains(file_name):
     - trains (list): Список поездов.
 
     """
-    with open(file_name, "r", encoding="utf-8") as fin:
-        loaded_data = json.load(fin)
+    if os.path.exists(file_name):
+        with open(file_name, "r", encoding="utf-8") as fin:
+            loaded_data = json.load(fin)
 
-    with open('scheme.json', 'r', encoding='utf-8') as scheme_file:
-        scheme = json.load(scheme_file)
+        with open('scheme.json', 'r', encoding='utf-8') as scheme_file:
+            scheme = json.load(scheme_file)
 
-    try:
-        jsonschema.validate(loaded_data, scheme)
-        return loaded_data
-    except jsonschema.exceptions.ValidationError as e:
-        print('Ошибка валидации данных:', e)
-        return None
+        try:
+            jsonschema.validate(loaded_data, scheme)
+            return loaded_data
+        except jsonschema.exceptions.ValidationError as e:
+            print('Ошибка валидации данных:', e)
+            return None
+    else:
+        return []
 
 
-def display_trains(trains):
+@click.group()
+@click.version_option('1.0.0')
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('filename', type=click.Path())
+@click.option('-n', '--number', type=int, prompt=True, help='The number of a train')
+@click.option('-d', '--destination', prompt=True, help='Destination point')
+@click.option('-st', '--start_time', prompt=True, help='Depart time')
+def add(filename, num, destination, start_time):
+    """
+    Добавляет информацию о поезде в список trains.
+
+    Args:
+    - trains (list): Список поездов.
+    - num (int): Номер поезда.
+    - destination (str): Пункт назначения.
+    - start_time (str): Время отправки
+
+    Returns:
+    - trains (list): Список поездов.
+    """
+    trains = load_trains(file_name=filename)
+
+    trains.append({
+        'num': num,
+        'destination': destination,
+        'start_time': start_time
+    })
+    if len(trains) > 1:
+        trains.sort(key=lambda item: item['start_time'])
+
+    save_trains(file_name=filename, trains=trains)
+
+
+@cli.command()
+@click.argument('filename', type=click.Path())
+def display(filename):
     """
     Выводит список поездов на экран.
 
@@ -85,7 +103,7 @@ def display_trains(trains):
     header = f"| {'№ поезда':^15} | {'Пункт назначения':^30} | {'Время отъезда':^25} |"
     print(header)
     print(line)
-    for train in trains:
+    for train in load_trains(file_name=filename):
         num = train.get('num', randint(1000, 10000))
         destination = train.get('destination', 'None')
         start_time = train.get('start_time', 'None')
@@ -94,7 +112,10 @@ def display_trains(trains):
     print(line)
 
 
-def select_trains(trains, destination):
+@cli.command()
+@click.argument('filename', type=click.Path())
+@click.option('-D', '--destination', prompt=True, help='The required destination')
+def select(filename, destination):
     """
     Выводит информацию о поездах, направляющихся в указанный пункт.
 
@@ -107,106 +128,11 @@ def select_trains(trains, destination):
 
     """
 
-    return [train for train in trains if train['destination'].strip() == destination]
+    return [train for train in load_trains(file_name=filename) if train['destination'].strip() == destination]
 
 
 def main(command_line=None):
-    # Creating file parser
-    file_parser = argparse.ArgumentParser(add_help=False)
-    file_parser.add_argument(
-        "filename",
-        action="store",
-        help="The data file name"
-    )
-
-    # Main parser of command line
-    parser = argparse.ArgumentParser("trains")
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s 1.0.0"
-    )
-
-    # Subparsers
-    subparsers = parser.add_subparsers(dest="command")
-
-    # Subparser for add command
-    add = subparsers.add_parser(
-        "add",
-        parents=[file_parser],
-        help="Add a new train"
-    )
-    add.add_argument(
-        "-n",
-        "--number",
-        action="store",
-        required=True,
-        type=int,
-        help="The number of a train"
-    )
-    add.add_argument(
-        "-d",
-        "--destination",
-        action="store",
-        required=True,
-        help="Destination point"
-    )
-    add.add_argument(
-        "-st",
-        "--start_time",
-        action="store",
-        required=True,
-        help="Depart time"
-    )
-
-    # Subparser for display command
-    display = subparsers.add_parser(
-        "display",
-        parents=[file_parser],
-        help="Display all trains"
-    )
-
-    # Subparser for select command
-    select = subparsers.add_parser(
-        'select',
-        parents=[file_parser],
-        help='Select trains by destination'
-    )
-
-    select.add_argument(
-        "-D",
-        "--dest",
-        action="store",
-        required=True,
-        help="The required destination"
-    )
-
-    args = parser.parse_args(command_line)
-    # Загрузить всех работников из файла, если файл существует.
-    is_dirty = False
-    if os.path.exists(args.filename):
-        trains = load_trains(args.filename)
-    else:
-        trains = []
-
-    match args.command:
-        case 'add':
-            trains = add_train(
-                trains,
-                args.number,
-                args.destination,
-                args.start_time
-            )
-            is_dirty = True
-        case 'display':
-            display_trains(trains)
-        case 'select':
-            selected = select_trains(trains, args.dest)
-            display_trains(selected)
-
-    # Save changes in file if data is changed
-    if is_dirty:
-        save_trains(args.filename, trains)
+    cli()
 
 
 if __name__ == '__main__':
